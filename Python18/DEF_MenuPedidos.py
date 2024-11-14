e007c044930a436faf15415469c0a681b5dcecef
raulbntzz <raul.benitezmulas24@campusfp.es>
2024-11-14 12:58:59 +0100

    Actualización DEF_MenuPedidos.py

--- Python18/DEF_MenuPedidos.py
+++ Python18/DEF_MenuPedidos.py
@@ -36,41 +36,30 @@ def main():
                 match elegir:
                     case 1:
                         DEF_MainCategoria.crear_categoria()
-                        elegir = elegir_opcion()
                     case 2:
                         DEF_MainCategoria.leer_categorias()
-                        elegir = elegir_opcion()
                     case 3:
                         DEF_MainCategoria.actualizar_categoria()
-                        elegir = elegir_opcion()
                     case 4:
                         DEF_MainCategoria.eliminar_categoria()
-                        elegir = elegir_opcion()
                     case 5:
                         DEF_MainCategoria.finalizar_programa()
-                        elegir = elegir_opcion()
                     case _:
                         print("Porfavor introduzca una opción válida")
-                        elegir = elegir_opcion()
             case 2:
                 mostrar_menu()
                 elegir = int(input("Introduce una opción del menú: "))
                 match elegir:
                     case 1:
-                        DEF_MainPedidos.crear_cliente()
-                        elegir = elegir_opcion()
+                        DEF_MainPedidos.crear_pedido()
                     case 2:
-                        DEF_MainPedidos.leer_cliente()
-                        elegir = elegir_opcion()
+                        DEF_MainPedidos.leer_pedido()
                     case 3:
-                        DEF_MainPedidos.actualizar_cliente()
-                        elegir = elegir_opcion()
+                        DEF_MainPedidos.actualizar_pedido()
                     case 4:
-                        DEF_MainPedidos.eliminar_cliente()
-                        elegir = elegir_opcion()
+                        DEF_MainPedidos.eliminar_pedido()
                     case 5:
                         DEF_MainPedidos.finalizar_programa()
-                        elegir = elegir_opcion()
                     case _:
                         print("Porfavor introduzca una opción válida")
                         elegir = elegir_opcion()
@@ -80,19 +69,14 @@ def main():
                 match elegir:
                     case 1:
                         DEF_MainClientes.crear_cliente()
-                        elegir = elegir_opcion()
                     case 2:
                         DEF_MainClientes.leer_cliente()
-                        elegir = elegir_opcion()
                     case 3:
                         DEF_MainClientes.actualizar_cliente()
-                        elegir = elegir_opcion()
                     case 4:
                         DEF_MainClientes.eliminar_cliente()
-                        elegir = elegir_opcion()
                     case 5:
                         DEF_MainClientes.finalizar_programa()
-                        elegir = elegir_opcion()
                     case _:
                         print("Porfavor introduzca una opción válida")
                         elegir = elegir_opcion()
@@ -102,19 +86,14 @@ def main():
                 match elegir:
                     case 1:
                         DEF_MainProducto.crear_producto()
-                        elegir = elegir_opcion()
                     case 2:
                         DEF_MainProducto.leer_producto()
-                        elegir = elegir_opcion()
                     case 3:
                         DEF_MainProducto.actualizar_producto()
-                        elegir = elegir_opcion()
                     case 4:
                         DEF_MainProducto.eliminar_producto()
-                        elegir = elegir_opcion()
                     case 5:
                         DEF_MainProducto.finalizar_programa()
-                        elegir = elegir_opcion()
                     case _:
                         print("Porfavor introduzca una opción válida")
                         elegir = elegir_opcion()
