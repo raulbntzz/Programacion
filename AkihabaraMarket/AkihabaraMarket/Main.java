70ce839ca33c2187e316201483a6ebb166aa3bc9
raulbntzz <raul.benitezmulas24@campusfp.es>
2025-06-12 10:18:30 +0200

    Delete AkihabaraMarket directory

--- AkihabaraMarket/AkihabaraMarket/Main.java
+++
@@ -1,8 +0,0 @@
-import com.akihabara.market.view.*; //Importamos los archivos
-
-public class Main {
-	public static void main(String[] args) {
-		InterfazConsola interfaz = new InterfazConsola();
-		interfaz.mostrarMenu();
-	}
-}
